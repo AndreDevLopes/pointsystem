291b3928e334a2b4f9a294c167597e3dbe364117
AndreDevLopes <andrelopessfla@gmail.com>
2022-05-14 17:24:01 -0300

    ajuste no nome do servidor

--- ponto/admin.py
+++ ponto/admin.py
@@ -6,7 +6,11 @@ from import_export.admin import ExportMixin
 # Register your models here.
 @admin.register(Ponto)
 class AdminPonto(ExportMixin, admin.ModelAdmin):
-    list_display = ['servidor', 'dia', 'entrada', 'intervalo', 'retorno', 'saida']
+    list_display = ['nome_do_servidor', 'dia', 'entrada', 'intervalo', 'retorno', 'saida']
     list_filter = (
         'dia',
     )
+
+    @admin.display(empty_value='???')
+    def nome_do_servidor(self, obj):
+        return obj.servidor.usuario.first_name
