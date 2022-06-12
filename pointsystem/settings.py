4a16fc7bd37b9dce4afde69fa0bb8c91e57b1216
AndreDevLopes <andrelopessfla@gmail.com>
2022-06-12 16:53:49 -0300

    chore: configuração dos arquivos estaticos

--- pointsystem/settings.py
+++ pointsystem/settings.py
@@ -172,9 +172,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MEDIA_DIR = BASE_DIR / 'media'
+MEDIA_DIR = BASE_DIR / 'staticfiles/media'
 MEDIA_ROOT = MEDIA_DIR
-MEDIA_URL = '/media/'
+MEDIA_URL = '/staticfiles/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
